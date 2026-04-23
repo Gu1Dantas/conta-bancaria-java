3c3c7551c6f8390ebb932ea1c645babad851e3af
Guilherme Dantas <143549745+Gu1Dantas@users.noreply.github.com>
2026-04-23 16:39:03 -0400

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nome = "Guilherme Dantas";
+        String TipoDeconta = "Corrente";
+        double saldo = 1900.00;
+        int opcao = 0;
+
+        System.out.println("========================");
+        System.out.println("Nome:" +nome);
+        System.out.println("Tipo de conta:" +TipoDeconta);
+        System.out.println("Saldo:" +saldo);
+        System.out.println("========================");
+
+        String menu = """
+                ==Escolha uma opção==
+                1 - Consultar saldo
+                2 - Tranferir valor
+                3 - Receber valor
+                4 - sair
+                """;
+
+        while (opcao !=4){
+            System.out.println(menu);
+            opcao = scan.nextInt();
+
+            if (opcao == 1){
+                System.out.println("Seu saldo atualmente é de: "+saldo);
+            } else if (opcao == 2){
+                System.out.println("informe o valor da trasnfencia: ");
+                double valor = scan.nextDouble();
+                if (saldo < valor){
+                    System.out.println("Saldo insuficiente para essa transação");
+                }   else {
+                    saldo -= valor;
+                    System.out.println("Seu saldo agora é de: "+saldo);
+                }
+            } if (opcao == 3){
+                System.out.println("Valor Recebido: ");
+                double valorRecebido = scan.nextDouble();
+                saldo += valorRecebido;
+                System.out.println("Novo Saldo: "+saldo);
+
+            } else if (opcao == 4){
+                System.out.println("Saindo...");
+            }
+        }
+
+    }
 }
